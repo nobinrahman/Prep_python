04b76796ee6586669482bb8279f659e0cf3b03f5
nobinrahman <168522348+nobinrahman@users.noreply.github.com>
2024-05-28 23:09:44 -0700

    Update Bubble sort algorithm.py

--- Bubble sort algorithm.py
+++ Bubble sort algorithm.py
@@ -71,6 +71,41 @@ output_string = reverse_vowels(input_string)
 print(output_string)  # Output: odicatuen
 
 
+##### 3 #####
 
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
+# Note that you must do this in-place without making a copy of the array.
 
+ 
+
+# Example 1:
+
+# Input: nums = [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+# Example 2:
+
+# Input: nums = [0]
+# Output: [0]
+
+def moving_zero_to_right(nums):
+	count = 0 
+	for i in range(len(nums)):
+		if nums[i] != 0:
+			nums[count],nums[i] = nums[i],nums[count]
+			count = count + 1
+	return nums
+nums = [0, 1, 0, 3, 12]
+x = moving_zeros_to_right(nums)
+print(x)
+
+def moving_zero_to_left(nums):
+	count = 0 
+	for i in range(len(nums)-1, -1, -1):
+		if nums[i] != 0:
+			nums[count],nums[i] = nums[i],nums[count]
+			count = count - 1
+	return nums
+nums = [0, 1, 0, 3, 12]
+x = moving_zeros_to_left(nums)
+print(x)
